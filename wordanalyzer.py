1c78e8bdf395967464881b782d22a7698087f668
wonbird <wonbird@gmail.com>
2016-10-10 05:30:54 +0900

    update

--- wordanalyzer.py
+++ wordanalyzer.py
@@ -1,9 +1,9 @@
 from _segmentator import *
 from _stopwords import *
 
-amode = 'D'  # 'F': Full mode, 'D': Default mode, 'S': cut_for_search mode, 'E': Extract tags mode
+amode = 'E'  # 'F': Full mode, 'D': Default mode, 'S': cut_for_search mode, 'E': Extract tags mode
 pmode = 'D'  # 'P': Print mode, 'D': Database mode
-topK = 30
+topK = 50
 
 doc_infos = get_document()
 stop_words = get_stop_words('cn')
